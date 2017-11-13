07beb07a33ee62ce342f3cab7c1019ba7782b985
Cameron Erdogan <cameron.the.erdogan@gmail.com>
2017-11-13 15:56:58 -0500

    added zombies that move towards you

---
+++ The Talking Dead/Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+
+	public float speed = 1f;
+	private Transform player;
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		float oldY = transform.position.y;
+		transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+		transform.position = new Vector3 (transform.position.x, oldY, transform.position.z);
+		transform.LookAt (player);
+	}
+}
---
+++ The Talking Dead/Assets/Scripts/ZombieFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieFactory : MonoBehaviour {
+
+	public float radius;
+	public float spawnRate;
+	public GameObject zombiePrefab;
+
+	private float spawnArcAngle = 180f;
+	private float currentTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		if (currentTime > spawnRate) {
+			spawnZombie ();
+			currentTime = 0;
+		}
+	}
+
+	void spawnZombie(){
+		print ("spawning zombie");
+		Vector3 spawnPoint = Vector3.forward * radius + Vector3.up * 0.4f;
+		float randomAngle = Random.value * spawnArcAngle - spawnArcAngle / 2f;
+
+		spawnPoint = Quaternion.Euler (0, randomAngle, 0) * spawnPoint;
+		GameObject thisZombie = Object.Instantiate (zombiePrefab, spawnPoint, Quaternion.identity) as GameObject;
+		thisZombie.transform.LookAt (GameObject.FindGameObjectWithTag ("Player").transform);
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere (transform.position, radius);
+	}
+}
