1f042fb1f736d5a6e6b865db9486142e0146646d
Grace Guo <graceguosiqi@gmail.com>
2017-11-30 13:25:36 -0500

    viewer UI and thumb model

--- The Talking Dead/Assets/DisplayZombieCount.cs
+++ The Talking Dead/Assets/DisplayZombieCount.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisplayZombieCount : MonoBehaviour {
+public class DisplayZombieCount : MonoBehaviour
+{
 
-    public GameManager gm;
-    TextMesh tm;
+	public GameManager gm;
+	TextMesh tm;
 
 	// Use this for initialization
-	void Start () {
-        tm = GetComponent<TextMesh>();
+	void Start ()
+	{
+		tm = GetComponent<TextMesh> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if(tm.text != gm.zombieKilled.ToString())
-        {
-            tm.text = gm.zombieKilled.ToString();
-        }
+	void Update ()
+	{
+		if (gm) {
+			if (tm.text != gm.zombieKilled.ToString ()) {
+				tm.text = gm.zombieKilled.ToString ();
+			}
+		}
 	}
 }
