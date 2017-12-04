b99e5d19c84e556f51bad08c9ceb1a309f11964c
nananag <32268204+nananag@users.noreply.github.com>
2017-12-04 14:08:52 -0800

    fixed timescale thing

--- The Talking Dead/Assets/Scripts/GameManager.cs
+++ The Talking Dead/Assets/Scripts/GameManager.cs
@@ -105,7 +105,6 @@ public class GameManager : MonoBehaviour
 	public void ZombieAttackedPlayer (WordZombie zombie)
 	{
 		Debug.Log ("zombie killed you! " + zombie.GetWord ());
-		Time.timeScale = 0;
 
 		print ("zombies killed: " + zombieKilled);
 
